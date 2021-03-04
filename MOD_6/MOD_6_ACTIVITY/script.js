4ffb5627b46e38b60ee24b537d4a42c2417cc04c
Brendan Tyler <bkrendan12@gmail.com>
2021-03-03 20:10:28 -0500

    erased useless javascript that was serving no purpose

--- MOD_6/MOD_6_ACTIVITY/script.js
+++ MOD_6/MOD_6_ACTIVITY/script.js
@@ -1,15 +1,4 @@
 // Assignment Code
-let growBtn = document.getElementById("#growbutton");
-let blueBtn = document.getElementById("#bluebutton");
-let fadeBtn = document.getElementById("#fadebutton");
-let resetBtn = document.getElementById("#resetbutton");
-
-function growBox() {
-    document.getElementById("growbutton").addEventListener("click", function(){
-
-        document.getElementById("box").style.height="350px";  
-    });
-}
  
 document.getElementById("growbutton").addEventListener("click", function(){
 
